42ff8352f10456a7dd0662a4bfb9c508c3edfa50
Blake Freer <59676067+BlakeFreer@users.noreply.github.com>
2025-11-29 15:25:26 -0500

    OV7670 Camera Streaming (#8)
    * Camera Stream Demo
    
    Arduino generates fake image data.
    
    Python App displays the data with opencv
    
    * rename arduino project
    
    * Start OV7670 Reader
    
    - Arduino IO configuration
    - OV7670 register addresses and defaults
    
    * SCCB Communication with OV7670
    
    - Read and write registers.
    - Repeatedly reads the product identifier and sends it over serial. View
      the serial output with `serial-dump/dump.py`
    - Doesn't read camera images yet
    
    Future Improvements:
    
    The SCCB functions are blocking. A read takes 300 us and write takes 450
    us. How can we use interrupts to make this non-blocking?
    
    The Read/Write functions check for bus arbitration which is unnecessary
    in a single-master circuit. See the comment in `ReadRegister()` about
    simplifying this.
    
    * OV7670 Camera Streaming
    
    Wire according to `pinout.txt`
    
    Upload firmware
    
    Run `apps/camera-stream/main.py`

---
+++ apps/camera-stream/main.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+
+import os
+import time
+import cv2
+import numpy as np
+import serial
+
+PORT = "/dev/ttyUSB0"
+BAUD = 2_000_000
+
+
+def main():
+    arduino = serial.Serial(PORT, baudrate=BAUD, timeout=5)
+
+    W = 640 + 10  # buffer for stray pixels? unsure why some rows are longer
+    H = 480
+
+    image = np.zeros((H, W), dtype=np.uint8)
+
+    for row in range(image.shape[0]):
+        image[row, :] = row % 255
+
+    row = 0
+
+    while True:
+        row_data = arduino.read_until(b"\0")[:-1]  # remove 0 byte
+        pixels = np.frombuffer(row_data, dtype=np.uint8)
+
+        try:
+            image[row, : pixels.size] = pixels
+            # draw inverted "cursor" line to show where the image is updating
+            image[(row + 1) % H, : pixels.size] = 255 - pixels
+
+        except ValueError:
+            continue
+
+        row = (row + 1) % H
+
+        cv2.imshow("Stream", image)
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord("q"):  # quit application
+            break
+
+        elif key == ord("r"):  # reset first row
+            row = 0
+
+        elif key == ord("s"):  # save image
+            os.makedirs("captures/", exist_ok=True)
+            timestamp = time.strftime("%Y-%d-%m_%H%M%S")
+            file = f"captures/{timestamp}.png"
+            cv2.imwrite(file, image)
+            print(f"Saved to {file}")
+
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
