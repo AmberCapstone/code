cae0f837d910cd59c95c5dcaf02fe7768188db3a
Blake Freer <bkfreer10@gmail.com>
2025-12-14 10:56:26 -0500

    Fix cobs compiler error

--- lib/cobs/cobs.hpp
+++ lib/cobs/cobs.hpp
@@ -5,6 +5,13 @@
 
 namespace amber::cobs {
 
+constexpr size_t MaxEncodedLength(size_t raw_length) {
+    constexpr size_t LEADING_ZERO = 1;
+    constexpr size_t TERMINATING_ZERO = 1;
+    size_t MAX_STUFF_BYTES = (raw_length + 253) / 254;
+    return LEADING_ZERO + raw_length + MAX_STUFF_BYTES + TERMINATING_ZERO;
+}
+
 class Decoder {
 public:
     Decoder(uint8_t* buffer);
@@ -17,8 +24,6 @@ private:
     uint8_t block_remaining_;
     uint8_t code_;
 };
-
-constexpr size_t MaxEncodedLength(size_t raw_length);
 
 size_t Encode(const uint8_t* raw, size_t length, uint8_t* output);
 
