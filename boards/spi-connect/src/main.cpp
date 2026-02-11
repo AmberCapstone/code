2e4be182af74237d19994a295acaad2c5441913b
Blake Freer <bkfreer10@gmail.com>
2026-02-11 11:14:51 -0500

    Rewrite spi-connect using Arduino libraries
    Rewrite SPI demo using Arduino libraries

--- boards/spi-connect/src/main.cpp
+++ boards/spi-connect/src/main.cpp
@@ -1,51 +1,51 @@
-#include <util/delay.h>
-#include <stdint.h>
-#include <avr/interrupt.h>
-
-#include "spi.hpp"
-#include "serial.hpp"
-
+#include "Arduino.h"
+#include "SPI.h"
+#include "cobs.hpp"
 
 using namespace amber;
 
 // // opcodes
-// // https://github.com/damdoy/ice40_ultraplus_examples/blob/master/spi_hw/README.md
+// //
+// https://github.com/damdoy/ice40_ultraplus_examples/blob/master/spi_hw/README.md
 // static constexpr uint8_t OPC_NOP = 0x00;
 // static constexpr uint8_t OPC_INIT = 0x01;
 // static constexpr uint8_t OPC_INV32 = 0x02;
 // static constexpr uint8_t OPC_LEDS = 0x04;
 
-static constexpr uint16_t MAX_LEN = MAX_PACKET_LEN;
+int spi_cs = 13;
+SPISettings spi_settings(100000, LSBFIRST, SPI_MODE0);
+
+uint8_t spi_buf[512];
+uint8_t tx_cobs_buf[cobs::MaxEncodedLength(512)];
 
 int main(void) {
-    spi::Config cfg;
-    cfg.lsb_first = true;
-    cfg.master = true;
-    cfg.mode = spi::Mode::Mode0;
-    cfg.clock_div = 128;
-    spi::Initialize(cfg);
+    pinMode(spi_cs, OUTPUT);
+    digitalWrite(spi_cs, HIGH);
 
-    serial::Initialize();
-    sei();
+    Serial.begin(500000);
+    SPI.begin();
 
-    _delay_ms(50);
-
-    static uint8_t tx[MAX_LEN]; 
-    static uint8_t rx[MAX_LEN];
+    cobs::Decoder<512> decoder;
 
     while (1) {
-        if (!serial::PacketReady()) {
-            continue;
+        // Receive COBS message from host
+        while (true) {
+            uint8_t byte = Serial.read();
+            if (decoder.Decode(&byte, 1)) {
+                break;
+            }
         }
+        // now know that decoder has a valid cobs message
 
-        uint16_t len = serial::ReadPacket(tx);
-        if (len == 0) {
-            continue; 
-        }
+        // perform a SPI transfer using decoder.
+        memcpy(spi_buf, decoder.buffer, decoder.length);
+        digitalWrite(spi_cs, LOW);
+        SPI.transfer(spi_buf, decoder.length);
+        digitalWrite(spi_cs, HIGH);
+        // spi_buf now holds the RX data from the FPGA
 
-        spi::PacketTransfer(tx, rx, len);
-
-        while (serial::IsBusy()) { }
-        serial::Transmit(rx, len);
+        // Send response to host
+        uint32_t tx_len = cobs::Encode(spi_buf, decoder.length, tx_cobs_buf);
+        Serial.write(tx_cobs_buf, tx_len);
     }
 }
