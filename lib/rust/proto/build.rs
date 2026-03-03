9cb0a9c103169debe9b1463340929a7681134d14
Blake Freer <bkfreer10@gmail.com>
2026-03-03 09:36:05 -0500

    Update protos for more submodules

--- lib/rust/proto/build.rs
+++ lib/rust/proto/build.rs
@@ -3,7 +3,14 @@ use std::{io::Result, path::Path};
 fn main() -> Result<()> {
     let proto_path = Path::new("../../../proto");
 
-    let protos = ["sensor/flash.proto", "sensor.proto"];
+    let protos = [
+        "sensor.proto",
+        "sensor/alerts.proto",
+        "sensor/camera.proto",
+        "sensor/flash.proto",
+        "sensor/measure.proto",
+        "sensor/parameters.proto",
+    ];
 
     prost_build::Config::new()
         .include_file("_proto_include.rs")
