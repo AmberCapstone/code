b615ed1deab4ea6391a93fcfe5443b0846ff5adc
Blake Freer <bkfreer10@gmail.com>
2026-03-03 22:52:38 -0500

    sensor-rs: Set up toolchain and protobuf compiler
    femtopb was incomplete
    prost needs an allocator
    
    micropb seems to work (or at least compile)

--- boards/sensor-rs/src/main.rs
+++ boards/sensor-rs/src/main.rs
@@ -3,16 +3,41 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::gpio::{self};
+use embassy_stm32::spi;
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
+
+mod proto {
+    #![allow(clippy::all)]
+    #![allow(nonstandard_style, unused, irrefutable_let_patterns)]
+    include!(concat!(env!("OUT_DIR"), "/generated_proto.rs"));
+}
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
-    info!("Hello World!");
 
-    let mut led = Output::new(p.PA5, Level::High, Speed::Low);
+    let mut led = gpio::Flex::new(p.PA5);
+
+    led.set_as_analog();
+
+    let mut mosi = p.PC12;
+    let mut miso = p.PC11;
+    let mut sck = p.PC10;
+
+    let flash_spi = spi::Spi::new_blocking(
+        p.SPI3,
+        sck.reborrow(),
+        mosi.reborrow(),
+        miso.reborrow(),
+        spi::Config::default(),
+    );
+    drop(flash_spi);
+
+    let mut mosi = gpio::Flex::new(mosi);
+    mosi.set_as_analog();
+    mosi.set_high();
 
     loop {
         info!("high");
