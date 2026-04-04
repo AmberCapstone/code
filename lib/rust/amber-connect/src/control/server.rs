39f8ec13db510a92e25ac2603aebb2be8d1f5d7f
Blake Freer <bkfreer10@gmail.com>
2026-04-03 23:29:51 -0400

    Improve `sensor-server` errors and flow
    Replace unwraps with error messages. Should solve #54
    
    Restructure sensor-server main to better use tokio async functions.
    
    Add timeouts in flasher and capture

--- lib/rust/amber-connect/src/control/server.rs
+++ lib/rust/amber-connect/src/control/server.rs
@@ -3,6 +3,7 @@ use std::time::Duration;
 use proto::sensor::Command;
 use tokio::sync::mpsc;
 use tokio_util::sync::CancellationToken;
+use tracing::{error, info, warn};
 use zeromq::{RepSocket, Socket, ZmqResult};
 
 use super::{
@@ -26,39 +27,44 @@ pub async fn run(
     lease_lifetime: Duration,
     stop: CancellationToken,
 ) -> ZmqResult<()> {
+    info!("starting control server");
     let mut lease = Lease::new(lease_lifetime);
 
     let mut socket = RepSocket::new();
     socket.bind(endpoint::COMMAND).await?;
 
-    loop {
-        tokio::select! {
-            request = socket.recv_json::<Request<Command>>() => {
-                let response: Response = match request {
-                    Ok(Request::Acquire{ name }) => {
-                        Response::Acquire(lease.acquire(&name).map(Into::into))
-                    }
-                    Ok(Request::Release { token }) => Response::Release(lease.withdraw(token)),
-                    Ok(Request::Renew{ token }) => Response::Renew(lease.renew(token).map(|h| h.expiry)),
-                    Ok(Request::Send { token, item }) => {
-                        if lease.is_owned_by(token) {
-                            commands_tx.send(item).await.expect("channel is alive");
-                            Response::Send(Ok(()))
-                        } else {
-                            Response::Send(Err(WrongToken))
-                        }
-                    },
-                    Err(JsonSocketError::EmptyFrame | JsonSocketError::Serde(_)) => Response::InvalidRequest,
-                    Err(JsonSocketError::Socket(z)) => {
-                        eprintln!("ZMQ error: {z:?}");
-                        Response::InvalidRequest
-                    }
-                };
+    stop.run_until_cancelled(async {
+        loop {
+            let request = socket.recv_json::<Request<Command>>().await;
 
-                socket.send_json(&response).await.unwrap();
+            let response: Response = match request {
+                Ok(Request::Acquire { name }) => Response::Acquire(lease.acquire(&name).map(Into::into)),
+                Ok(Request::Release { token }) => Response::Release(lease.withdraw(token)),
+                Ok(Request::Renew { token }) => Response::Renew(lease.renew(token).map(|h| h.expiry)),
+                Ok(Request::Send { token, item }) => {
+                    if lease.is_owned_by(token) {
+                        commands_tx.send(item).await.expect("channel is alive");
+                        Response::Send(Ok(()))
+                    } else {
+                        Response::Send(Err(WrongToken))
+                    }
+                }
+                Err(JsonSocketError::EmptyFrame | JsonSocketError::Serde(_)) => Response::InvalidRequest,
+                Err(JsonSocketError::Socket(z)) => {
+                    eprintln!("ZMQ error: {z:?}");
+                    Response::InvalidRequest
+                }
+            };
+
+            if let Err(e) = socket.send_json(&response).await {
+                warn!(err=?e, "failed to send json response");
             }
-            () = stop.cancelled() => { break; }
-        };
+        }
+    })
+    .await;
+
+    for err in socket.close().await {
+        error!(err=?err, "error while closing socket");
     }
 
     Ok(())
